3808d9a29e8b96be4e1d2454f210942ae4e75ffb
MirunaMb <mirunamarcela98@gmail.com>
2023-07-26 17:47:20 +0200

    add esercitazione

--- main.js
+++ main.js
@@ -1,7 +1,56 @@
-const firstNumber = parseInt(prompt("Inserisci un numero"));
+// const firstNumber = parseInt(prompt("Inserisci un numero"));
 
-const secondNumber = parseInt(prompt("Inserisci un numero"));
+// const secondNumber = parseInt(prompt("Inserisci un numero"));
  
-const randomNumber = Math.floor(Math.random () * firstNumber)+secondNumber;
+// const randomNumber = Math.floor(Math.random () *(secondNumber - firstNumber))  + firstNumber;
 
-console.log (randomNumber);
+// console.log (randomNumber);
+
+
+// EX 2
+// const firstWord = prompt("Inserisci una parola");
+// const secWord = prompt("Inserisci un'altra parola")
+// 
+// if ( firstWord.length = secWord.length){
+//  console.log(firstWord , secWord)
+// } else if {
+    // (firstWord.length > secWord.length)
+    // console.log(firstWord)
+// }else {
+    // console.log(secWord);
+// }
+
+// EX 3
+// 1.CREO UN ARRAY VUOTO 
+// const contenitore =[];
+
+// 2. CREO LA VARIABILE PER CONTENERE LA SOMMA
+// let somma = 0;
+
+// 3.CONTINUA A CHIEDERE I NUMERI ALL UTENTE
+
+// while (somma < 50) {
+    // let userNumber = parseInt(prompt("Inserisci un numero"));
+    // somma += userNumber ;
+// 
+    // if (somma < 50) {
+        // contenitore.push(userNumber);
+    // }
+// }
+// console.log(contenitore);
+
+
+// Ex 4
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    // let evenNumbers = [];
+    // for(let i = 0; i < numbers.length -1; i++){
+        // if (numbers[i] % 2 == 0){
+            // evenNumbers.push(i)
+        // }
+    // }
+    // console.log(evenNumbers);
+
+
+
+
+
